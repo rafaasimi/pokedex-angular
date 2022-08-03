97644e385a251cc81f4859414346781f80643b44
Rafael Simionato Zorzenon <rafael_zorzenon@sicredi.com.br>
2022-08-03 14:26:37 -0300

    Criado estilização inicial para visualização dos cards em tela

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UtilsService } from '../shared/utils.service';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() numPokemon: string = '';
+
+  constructor(public utilsService: UtilsService) { }
 
   ngOnInit(): void {
   }
